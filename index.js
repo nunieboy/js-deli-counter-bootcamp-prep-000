4ce20b0c487cfda8f0e426ae5970a6b5052d5968
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-01 04:22:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,25 +13,27 @@ function nowServing(katzDeliLine){
   }
 }
 
-//function currentLine(katzDeliLine){
-//  var line = []
-//  if(katzDeliLine.length > 0){
-//    for(let i = 0; i < katzDeliLine.length; i++){
-//      line.push(i+1 + ". " + katzDeliLine[i])
-//    }return `The line is currently: ${line}`
-//  }else{ 
-//    return `The line is currently empty.`
-//  }
-//}
-
-
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length !== 0){
-    var line = []
-    var i = 1
-    do {line.push(i++)} while (katzDeliLine.length !== 0)
-    return `${line}`
-  }else{
-     return `The line is currently empty.`
-   }
+  var line = []
+  
+  k = 0
+  if(katzDeliLine.length > 0){
+    for(let l = 1; i < katzDeliLine.length +1; i++){
+      line.push(l)
+    }return `The line is currently: ${line}`
+  }else{ 
+    return `The line is currently empty.`
+  }
+}
+
+
+//function currentLine(katzDeliLine){
+//  if(katzDeliLine.length !== 0){
+//    var line = []
+//    var i = 1
+//    do {line.push(i++)} while (katzDeliLine.length !== 0)
+//    return `${line}`
+//  }else{
+//     return `The line is currently empty.`
+//   }
 }

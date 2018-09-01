52a9b379ebc5f5330ae4dfc0fd830ba83aee14d9
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-01 04:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine){
   var line = []
   if(katzDeliLine.length > 0){
     for(let i = 0; i < katzDeliLine.length; i++){
-      line.push(`${i+1} + ". " + ${katzDeliLine[i]}`)
+      line.push(`${i+1} ${katzDeliLine[i]}`)
     }return `The line is currently:${line}`
   }else{ 
     return `The line is currently empty.`
